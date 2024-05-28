1fbde5d72b98bb2c5cb5d5b717878d8a9268efb0
kwkry <vincekurt1@gmail.com>
2024-05-28 11:31:56 +0800

    Removed status bar and utilize orientation in app.json instead for screen orientation, changed background color of custom tab bar to white

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-import * as ScreenOrientation from "expo-screen-orientation";
 import RootNavigator from "./navigators/RootNavigator";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-  return (
-    <>
-      <StatusBar backgroundColor="#FF5C00" />
-      <RootNavigator />
-    </>
-  );
+  return <RootNavigator />;
 }
