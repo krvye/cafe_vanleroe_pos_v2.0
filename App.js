9f6aad06918294816b862bf0b88a5e72b1be0260
kwkry <vincekurt1@gmail.com>
2024-06-04 01:12:43 +0800

    Added env var in workflows and screen orientation in App

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import RootNavigator from "./navigators/RootNavigator";
 import { StatusBar } from "expo-status-bar";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor="white" />
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
       <RootNavigator />
     </>
   );
