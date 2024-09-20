b6de7183576dc250cc05a0ea1fa2d48adcc5b5c8
janeblanca <janeblancaocampo@gmail.com>
2024-09-20 18:01:58 +0800

    updated app.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import RootNavigator from "./navigators/RootNavigator";
 import { StatusBar } from "expo-status-bar";
-import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
-  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
-
-  return <RootNavigator />;
+  return (
+    <>
+      <StatusBar backgroundColor="white" />
+      <RootNavigator />
+    </>
+  );
 }
