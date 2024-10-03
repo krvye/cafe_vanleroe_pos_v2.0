acc3b1fd54e3ea0bd76bb6652f3fb19ee18d4687
janeblanca <janeblancaocampo@gmail.com>
2024-10-03 14:16:21 +0800

    updated firebaseConfig

--- services/firebase/firebaseConfig.js
+++ services/firebase/firebaseConfig.js
@@ -1,46 +1,24 @@
 // // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// ----- Try ko lang to - Jane --- 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBItS0OxfK-ORjOoaVZL4zTh78dDoeBxkk",
-  authDomain: "cvlr-intern-db.firebaseapp.com",
-  projectId: "cvlr-intern-db",
-  storageBucket: "cvlr-intern-db.appspot.com",
-  messagingSenderId: "420208905923",
-  appId: "1:420208905923:web:1ecb1401e4d45102371c76",
-  measurementId: "G-2XJKRDQH78"
+  apiKey: "AIzaSyB2Dn2gkmeRrIcxYwUJHXM_gL0m_FCufYM",
+  authDomain: "cafe-vanleroe-system-test.firebaseapp.com",
+  projectId: "cafe-vanleroe-system-test",
+  storageBucket: "cafe-vanleroe-system-test.appspot.com",
+  messagingSenderId: "827239060830",
+  appId: "1:827239060830:web:6be801f03615eed0f2abcf",
+  measurementId: "G-PEFZXED05H"
 };
 
-// Initialize Firebase
+
+// // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export default app; 
