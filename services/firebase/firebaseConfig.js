686c71baa73b724158b0ac92f972323ba9a1d1b4
krvye <vincekurt1@gmail.com>
2024-10-09 19:43:36 +0800

    Removed unnecessary commnets in firebase config

--- services/firebase/firebaseConfig.js
+++ services/firebase/firebaseConfig.js
@@ -1,12 +1,6 @@
-// // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB2Dn2gkmeRrIcxYwUJHXM_gL0m_FCufYM",
   authDomain: "cafe-vanleroe-system-test.firebaseapp.com",
@@ -17,9 +11,7 @@ const firebaseConfig = {
   measurementId: "G-PEFZXED05H",
 };
 
-// // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-// const analytics = getAnalytics(app);
 
 export { app, db };
